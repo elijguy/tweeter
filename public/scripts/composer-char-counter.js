8ba74fd0beaa70bb85a70dabbc87da1b1ef1200d
Elijah Guy <elijahguy17@yahoo.ca>
2017-11-14 23:11:34 +0000

    character counter working!

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,27 @@
+
+$( document ).ready(function() {
+
+  $('textarea').on('keypress', function(e) {
+
+    var char = $('textarea').val().length;
+
+    var max = 140;
+
+    var x = (max - char);
+
+    if(x < 0){
+
+      $(this).siblings('.counter').text(x).css("color","red");
+
+    }else{
+
+      $(this).siblings('.counter').text(x);
+
+    }
+
+  });
+
+});
+
+
+
